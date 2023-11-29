4bc65eaf2feba9351132ec6fea79fc49a4db283a
ckorley4 <ckorley4uo@yahoo.com>
2023-11-28 20:50:14 -0500

    Looping Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(array) {
+  const newarr = []
+  for (let me = 0; me < array.length; me++) {
+    newarr[me] = `Thank you, ${array[me]}, for the wonderful surprise gift!`
+  }
+  return newarr
+}
+
+function countDown(num) {
+  for (let me = num; me >= 0; me--) {
+    console.log(me)
+  }
+}
